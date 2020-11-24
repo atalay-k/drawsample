f2283ec9e0fd06753384d12475dbc8ede3d70ed9
Kubra ATALAY KABASAKAL <katalay@hacettepe.edu.tr>
2020-11-24 21:03:10 +0300

    Add NEWS.md

--- data-raw/constants_table.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `constants_table` dataset goes here
-
-usethis::use_data(constants_table, overwrite = TRUE)
--- data-raw/example_data.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `example_data` dataset goes here
-
-usethis::use_data(example_data, overwrite = TRUE)
